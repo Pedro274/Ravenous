4b9ebfec40571bc36d6a153c06acb2facbf35f00
Pedro Hernandez <Pedro.lin.ka@gmail.com>
2019-12-27 14:24:00 -0500

    last commit

---
+++ src/component/SearchBar/SearchBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import 'SearchBar.css';
+
+const sortByOptions = {
+    "Best Match":,
+    "Highest Rated":,
+    "Most Reviewed":,
+
+};
+
+
+class SearchBar extends React.Component {
+    
+    render() {
+        return (
+
+        )
+    }
+}
