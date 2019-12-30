49fcd650823a8f60835db680cfa68ada90046704
Pedro Hernandez <Pedro.lin.ka@gmail.com>
2019-12-30 14:57:40 -0500

    last commit part-1

--- src/component/Business/Business.js
+++ src/component/Business/Business.js
@@ -31,7 +31,7 @@ class Business extends React.Component {
                     </div>
                     <div className="Business-reviews">
                         <h3>{business.category}</h3>
-                        <h3 class="rating">{business.rating}</h3>
+                        <h3 className="rating">{business.rating}</h3>
                         <p>{business.reviewCount}</p>
                     </div>
                 </div>
--- src/component/SearchBar/SearchBar.js
+++ src/component/SearchBar/SearchBar.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import 'SearchBar.css';
+import './SearchBar.css';
 
 const sortByOptions = {
-    "Best Match": best_match,
-    "Highest Rated": rating,
-    "Most Reviewed": review_count,
+    "Best Match": 'best_match',
+    "Highest Rated": 'rating',
+    "Most Reviewed": 'review_count',
 
 };
 
@@ -12,15 +12,31 @@ const sortByOptions = {
 class SearchBar extends React.Component {
     
     renderSortByOptions(){
-        return 
-    }
+        return Object.keys(sortByOptions).map(sortByOption => {
+            const sortByOptionValue = sortByOptions[sortByOption];
+            return <li key={sortByOptionValue}>{sortByOption}</li>
+        })
+    };
 
 
     render() {
         return (
-
-        )
-    }
+                    <div className="SearchBar">
+                        <div className="SearchBar-sort-options">
+                            <ul>
+                                {this.renderSortByOptions()}
+                            </ul>
+                        </div>
+                        <div className="SearchBar-fields">
+                            <input placeholder="Search Businesses" />
+                            <input placeholder="Where?" />
+                        </div>
+                        <div className="SearchBar-submit">
+                            <a>Let's Go</a>
+                        </div>
+                    </div>
+                )
+            }
 };
 
 export default SearchBar
