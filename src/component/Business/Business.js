5353de25165b9b4c91a7f7ee368482a30dfa0d39
Pedro Hernandez <Pedro.lin.ka@gmail.com>
2020-01-06 12:40:28 -0500

    add props functionality

--- src/component/Business/Business.js
+++ src/component/Business/Business.js
@@ -2,37 +2,29 @@ import React from 'react';
 import './Business.css';
 
 
-const business = {
-    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90
-};
-
 class Business extends React.Component {
+    constructor(props){
+        super(props);
+        this.business = this.props.business
+    }
    
     render(){
         return (
             <div className="Business">
                 <div className="image-container">
-                    <img src={business.imageSrc} alt={business.name}/>
+                    <img src={this.business.imageSrc} alt={this.business.name}/>
                 </div>
-                <h2>{business.name}</h2>
+                <h2>{this.business.name}</h2>
                 <div className="Business-information">
                     <div className="Business-address">
-                        <p>{business.address}</p>
-                        <p>{business.city}</p>
-                        <p>{business.state + business.zipCode}</p>
+                        <p>{this.business.address}</p>
+                        <p>{this.business.city}</p>
+                        <p>{this.business.state + this.business.zipCode}</p>
                     </div>
                     <div className="Business-reviews">
-                        <h3>{business.category}</h3>
-                        <h3 className="rating">{business.rating}</h3>
-                        <p>{business.reviewCount}</p>
+                        <h3>{this.business.category}</h3>
+                        <h3 className="rating">{this.business.rating}</h3>
+                        <p>{this.business.reviewCount}</p>
                     </div>
                 </div>
             </div>
